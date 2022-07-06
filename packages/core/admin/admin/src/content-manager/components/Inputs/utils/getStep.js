ec490e39b3451d410cd853797fb87193ddf52786
Mickaël Gagné <mickael.oak@gmail.com>
2022-07-06 18:25:33 -0400

    Change default step for timepicker

--- packages/core/admin/admin/src/content-manager/components/Inputs/utils/getStep.js
+++ packages/core/admin/admin/src/content-manager/components/Inputs/utils/getStep.js
@@ -6,7 +6,7 @@ const getStep = type => {
   } else if (type === 'time' || type === 'datetime') {
     // Since we cannot set a value that is not in the list of the time picker, we need to set the step to 1
     // TODO: Fix the timepicker in order to be able to set any value regardless of the list
-    step = 1;
+    step = 5;
   } else {
     step = 1;
   }
